2046c60ad48ee62384c959b233799bf8e5187631
Rajmangukiya01 <rajmangukiya1002gmail.com>
2022-10-03 13:02:22 +0100

    My homework week 5

---
+++ src/homework/Program1.java
@@ -0,0 +1,12 @@
+package homework;
+
+//Write a program that prints your BIO Data
+
+public class Program1 {
+    public static void main(String []args) {
+        System.out.println("Name: Raj Mangukiya");
+        System.out.println("Birth Date: 1st May 1999");
+        System.out.println("Course: Software Testing");
+        System.out.println("Group: TestNG");
+    }
+}
---
+++ src/homework/Program10.java
@@ -0,0 +1,9 @@
+package homework;
+
+public class Program10 {
+    public static void main(String []args){
+        System.out.print("25 * 5=");
+        System.out.print(25*5);
+    }
+
+}
